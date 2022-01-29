fad993f8892e8de71d6af0e7cffd5422a1dc60d3
Your Namegit <jacob.sanders@cloudhybrid.io>
2022-01-28 20:58:09 -0600

    JSON Prompt

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import { Server } from "./index";
-import { Controller } from "./controllers";
 import { Middleware } from "./middleware";
+import { Controller } from "./controllers";
 
 const Application = Server();
 
 await Middleware(Application);
 
 Application.use("/", Controller);
-
-/// const Content = {
-///     Key: FS.readFileSync(Path.join(CWD, Process.env["TLS"]["Key"])),
-///     PFX: FS.readFileSync(Path.join(CWD, Process.env["TLS"]["PFX"])),
-///     Certificate: FS.readFileSync(
-///         Path.join(CWD, Process.env["TLS"]["Certificate"])
-///     )
-/// };
-
 
 Application.listen(3443, "0.0.0.0");
